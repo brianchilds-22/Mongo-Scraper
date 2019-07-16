80ff907ca141d9ba9b1e2b97e2249edae87e7432
brianchilds-22 <brianchilds22@msn.com>
2019-07-16 12:17:09 -0600

    starter files

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var mongoose = require("mongoose");
+var PORT = process.env.PORT || 3000;
+var app = express();
+var router = express.Router();
+app.use(express.static(__dirname + "/public"));
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+app.use(router);
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(db, function(error) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log("mongoose is connected");
+    }
+});
+app.listen(PORT, function() {
+    console.log("Listening on port:" + PORT);
+});
