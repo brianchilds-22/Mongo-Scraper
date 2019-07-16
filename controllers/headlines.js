fbec0417ae53c274ba7aebee9a6fbc8b80f850b0
brianchilds-22 <brianchilds22@msn.com>
2019-07-16 14:06:54 -0600

    headlines.js

--- controllers/headlines.js
+++ controllers/headlines.js
@@ -3,19 +3,36 @@ var makeDate = required("scripts\dates.js");
 var Headline = require("models\Headline");
 
 module.exports = {
-    fetch: function(cb) {
-        scrape(function(data) {
-        var articles = data;
-        for (var i=0; i < articles.length; i++) {
-            articles[i].date = makeDate();
-            articles[i].saved = false;
-        }
-        Headline.collection.insertMany(articles, {ordered:false}, function(err, docs){
-            cb(err,docs);
+    fetch: function (cb) {
+        scrape(function (data) {
+            var articles = data;
+            for (var i = 0; i < articles.length; i++) {
+                articles[i].date = makeDate();
+                articles[i].saved = false;
+            }
+            Headline.collection.insertMany(articles, {
+                ordered: false
+            }, function (err, docs) {
+                cb(err, docs);
+            });
         });
-    });
-}, 
-delete: function(query,cb) {
-    Headline.remove(query.cb);
-}
+    },
+    delete: function (query, cb) {
+        Headline.remove(query.cb);
+    },
+    get: function (query, cb) {
+        Headline.find(query)
+            .sort({
+                _id: -1
+            })
+            .exec(function (err, doc) {
+                cb(doc);
+            });
+    },
+    update: function(query,cb) {
+        Headline.update({_id: query._id}, {
+            $set: query
+        }, {}, cb);
+    }
+
 }
